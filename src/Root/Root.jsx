b7f2b4912ded2d2bdb93673e535914eee0104407
unknown <sohrawardy1998@gmail.com>
2024-08-24 21:57:51 +0600

    getting started with navbar

--- src/Root/Root.jsx
+++ src/Root/Root.jsx
@@ -1,8 +1,12 @@
 import { Outlet } from "react-router-dom";
+import Navbar from "../Navbar/Navbar";
+import Footer from "../Footer/Footer";
 const Root = () => {
   return (
     <div>
+      <Navbar></Navbar>
       <Outlet></Outlet>
+      <Footer></Footer>
     </div>
   );
 };
