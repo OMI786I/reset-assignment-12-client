f66edb8d64e30211c4812c3072e5628db72b8395
unknown <sohrawardy1998@gmail.com>
2024-08-24 23:15:56 +0600

    design of banner section

--- src/Component/Banner.jsx
+++ src/Component/Banner.jsx
@@ -6,12 +6,26 @@ const Banner = () => {
       <div
         className="hero min-h-screen"
         style={{
-          backgroundImage: "url(https://i.ibb.co/hgfVpMp/Banner.webp)",
+          backgroundImage: "url(https://i.ibb.co/kKHymG7/Banner.jpg)",
         }}
       >
-        <div className="hero-content text-neutral-content text-center">
+        <div className="hero-content text-neutral-content text-left">
           <div className="max-w-md">
-            <button className="btn btn-primary">Get Started</button>
+            <h1 className="mb-5 text-3xl md:text-5xl font-bold">
+              BE GRATEFUL AND DONATE BLOOD
+            </h1>
+            <p className="mb-5 md:text-2xl text-red-600">
+              Give Blood Save Life
+            </p>
+            <div className="flex gap-6">
+              {" "}
+              <button className="btn btn-error text-white">
+                Join as a donor
+              </button>
+              <button className="btn btn-outline text-white">
+                Search donor
+              </button>
+            </div>
           </div>
         </div>
       </div>
