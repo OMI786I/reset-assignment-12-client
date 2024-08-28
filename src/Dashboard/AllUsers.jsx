147a295f84464984e91d7052bc2b14e3a89622d8
OMI786I <sohrawardy1998@gmail.com>
2024-08-28 18:56:57 +0600

    getting started with all users

--- src/Dashboard/AllUsers.jsx
+++ src/Dashboard/AllUsers.jsx
@@ -1,4 +1,17 @@
+import { useQuery } from "@tanstack/react-query";
+
 const AllUsers = () => {
+  const { isPending, error, data } = useQuery({
+    queryKey: ["repoData"],
+    queryFn: () =>
+      fetch("http://localhost:5000/donor").then((res) => res.json()),
+  });
+  console.log(data);
+
+  if (isPending) return "Loading...";
+
+  if (error) return "An error has occurred: " + error.message;
+
   return <div>This is all users</div>;
 };
 
